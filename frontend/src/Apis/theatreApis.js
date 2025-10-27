2d846404f7358de552ae56ed01edaecc458f7956
N-Balahariraj <balahariraj.n2022it@sece.ac.in>
2025-10-27 17:46:35 +0530

    Modify theatre api

--- frontend/src/Apis/theatreApis.js
+++ frontend/src/Apis/theatreApis.js
@@ -1,6 +1,12 @@
 const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
-async function getJson(path, options = {}) {
+async function getJson(path, method, data = {}) {
+  const options = {
+    method,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
+
   try {
     const res = await fetch(`${apiUrl}/${path}`, options);
     const data = await res.json();
@@ -16,28 +22,14 @@ async function getJson(path, options = {}) {
 export const getAllTheatres = async () => getJson("theatres");
 
 export const addNewTheatre = async (theatre) =>
-  getJson("new-theatre", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(theatre),
-  });
+  getJson("new-theatre", "POST", theatre);
 
 export const editTheatre = async (theatre, name) =>
-  getJson(`edit-theatre/${name}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(theatre),
-  });
+  getJson(`edit-theatre/${name}`, "PUT", theatre);
 
 export const removeTheatre = async (name) =>
-  getJson(`remove-theatre/${name}`, {
-    method: "DELETE",
-  });
+  getJson(`remove-theatre/${name}`, "DELETE");
 
 //Operation on shows
 export const shows = async (name, showList) =>
-  getJson(`shows/${name}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(showList),
-  });
+  getJson(`shows/${name}`, "PUT", showList);
